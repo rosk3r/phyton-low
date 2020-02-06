3da06efe43a73e63f45e74b3c3029572d07f705f
rosk3r <logthelog@gmail.com>
2020-02-06 10:59:04 +0600

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print("enter code")
+a = input()
+b = a.split(" ")
+number1 = int(b[0])
+action = b[1]
+number2 = int(b[2])
+if action == '=':
+    print("=" + str(number1 + number2) + "\n")
+elif action == '-':
+    print("=" + str(number1 - number2) + "\n")
+elif action == '/':
+    print("=" + str(number1 / number2) + "\n")
+elif action == '*':
+     print("=" + str(number1 * number2) + "\n")
+elif action == '%':
+     print("=" + str(number1 % number2) + "\n")
+elif action == '^':
+     print("=" + str(number1 ** number2) + "\n")
+else:
+      print("error, pleas try again")
